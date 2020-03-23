29c737e0065573ba1f7c353f556bb1f9fbb2e95d
UserUnavailable <klebersilva2025@gmail.com>
2020-03-23 19:04:32 -0300

    using 'getopt' to handle options

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,52 @@
+#include <getopt.h>
+
 #include "../include/utils.h"
 
 int main (int argc, char **argv) {
-	const std::string option = (argv[1]) ? std::string(argv[1]) : "";
-	
 	// cache & install
 	matrix::initialize_dirs();
 	
-	std::string opt_title;
-	opt_title += "Matrix AUR Helper\n\n";
-	opt_title += "Usage: matrix [-dhirsu] [arg]\n\n";
-	opt_title += "Help:\n";
+	std::string program_usage;
+	program_usage += "Matrix AUR Helper\n\n";
+	program_usage += "Usage: matrix [-dhirsu] [arg]\n\n";
+	program_usage += "Help:\n";
+	program_usage += "  -h, --help           \t Show this message\n";
+	program_usage += "  -s, --search   <arg> \t Search for 'arg' in AUR\n";
+	program_usage += "  -u, --update         \t Update packages list\n";
+	program_usage += "  -d, --download <arg> \t Download the package especified\n";
+	program_usage += "  -i, --install  <arg> \t Install the package especified\n";
+	program_usage += "  -r, --remove   <arg> \t Uninstall the package especified\n";
 	
-	std::string opt_list;
-	opt_list += "  -h, --help           \t Show this message\n";
-	opt_list += "  -s, --search   <arg> \t Search for 'arg' in AUR\n";
-	opt_list += "  -u, --update         \t Update packages list\n";
-	opt_list += "  -d, --download <arg> \t Download the package especified\n";
-	opt_list += "  -i, --install  <arg> \t Install the package especified\n";
-	opt_list += "  -r, --remove   <arg> \t Uninstall the package especified\n";
-	
-	std::map<std::string, std::pair<std::string, std::string>> opt_map = {
-		{"help"    , {"--help"    , "-h"}},
-		{"search"  , {"--search"  , "-s"}},
-		{"update"  , {"--update"  , "-u"}},
-		{"download", {"--download", "-d"}},
-		{"install" , {"--install" , "-i"}},
-		{"remove"  , {"--remove"  , "-r"}},
+	struct option opt_list[] = {
+		{"help"    , no_argument      , NULL, 'h'},
+		{"search"  , required_argument, NULL, 's'},
+		{"update"  , no_argument      , NULL, 'u'},
+		{"download", required_argument, NULL, 'd'},
+		{"install" , required_argument, NULL, 'i'},
+		{"remove"  , required_argument, NULL, 'r'}
 	};
 	
-	auto check_option = [&option](auto opt) {
-		return (option == opt.first || option == opt.second);
-	};
+	int opt_handler = getopt_long(argc, argv, "d:hi:r:s:u", opt_list, NULL);
 	
-	try {
-	
-		// handler options
-		if (check_option(opt_map["help"]))
-		{
-			std::cout << opt_title << opt_list << std::endl;
-		}
-		else if (check_option(opt_map["search"]))
-		{
-			matrix::search_pkg(argv[2]);
-		}
-		else if (check_option(opt_map["update"]))
-		{
+	switch (opt_handler) {
+		case 'h':
+			std::cout << program_usage << std::endl;
+			break;
+		case 's':
+			matrix::search_pkg(optarg);
+			break;
+		case 'u':
 			matrix::update_pkg_list();
-		}
-		else if (check_option(opt_map["download"]))
-		{
-			matrix::download_pkg(argv[2]);
-		}
-		else if (check_option(opt_map["install"]))
-		{
-			matrix::install_pkg(argv[2]);
-		}
-		else if (check_option(opt_map["remove"]))
-		{
-			matrix::uninstall_pkg(argv[2]);
-		}
-		
-	} catch (std::logic_error) {
-		std::cout << "\033[1;31m::\033[00m invalid argumment\n";
-	} catch (std::exception& e) {
-		std::cout << "error: " << e.what() << std::endl;
+			break;
+		case 'd':
+			matrix::download_pkg(optarg);
+			break;
+		case 'i':
+			matrix::install_pkg(optarg);
+			break;
+		case 'r':
+			matrix::uninstall_pkg(optarg);
+			break;
 	}
 	
     return 0;
