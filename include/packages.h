6996dd51e4b5b5cac46a309981eae8cad930d52b
UserUnavailable <klebersilva2025@gmail.com>
2020-03-24 10:50:51 -0300

    splitted from utils.h

---
+++ include/packages.h
@@ -0,0 +1,15 @@
+#ifndef MATRIX_PACKAGES_H_
+#define MATRIX_PACKAGES_H_
+
+#include "../include/utils.h"
+
+namespace matrix {
+	// options
+	void search_pkg(std::string);
+	void update_pkg_list();
+	void download_pkg(std::string);
+	void install_pkg(std::string);
+	void uninstall_pkg(std::string);
+}
+
+#endif
