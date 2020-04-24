00d6b70f1abf16400314b143718dc15af71849af
UserUnavailable <klebersilva2025@gmail.com>
2020-04-23 22:24:24 -0300

    now it can update the version of packages

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,16 @@ int main(int argc, char **argv) {
     matrix::initialize_dirs();
 
     std::string usage_str =
-        "Matrix AUR Helper\n\n"
-        "Usage: matrix [-dhiqrsu] [arg]\n\n"
+        "Usage: matrix [-dhiqrsuy] [...]\n\n"
         "Help:\n"
         "  -h, --help           \t Show this message\n"
-        "  -s, --search   <arg> \t Search for 'arg' in AUR\n"
-        "  -u, --update         \t Update packages list\n"
-        "  -d, --download <arg> \t Download the package especified\n"
-        "  -i, --install  <arg> \t Install the package especified\n"
-        "  -r, --remove   <arg> \t Uninstall the package especified\n"
-        "  -q, --query    <arg> \t Show info about the package especified\n\n"
+        "  -s, --search   <pkg> \t Search for 'arg' in AUR\n"
+        "  -u, --update   <pkg> \t Update the package especified\n"
+        "  -d, --download <pkg> \t Download the package especified\n"
+        "  -i, --install  <pkg> \t Install the package especified\n"
+        "  -r, --remove   <pkg> \t Uninstall the package especified\n"
+        "  -q, --query    <pkg> \t Show info about the package especified\n"
+        "  -y, --sync           \t Sync the packages list\n\n"
         "  --ccache             \t Clear the cache directory\n"
         "  --version            \t Print this software version\n";
 
@@ -35,28 +35,29 @@ int main(int argc, char **argv) {
     struct option opt_list[] = {
         {"help"    , no_argument      , NULL, 'h'},
         {"search"  , required_argument, NULL, 's'},
-        {"update"  , no_argument      , NULL, 'u'},
+        {"update"  , required_argument, NULL, 'u'},
         {"download", required_argument, NULL, 'd'},
         {"install" , required_argument, NULL, 'i'},
         {"remove"  , required_argument, NULL, 'r'},
         {"query"   , required_argument, NULL, 'q'},
+        {"sync"    , no_argument      , NULL, 'y'},
         // long options only
         {"ccache" , no_argument, NULL, CCACHE},
         {"version", no_argument, NULL, VERSION},
         // dev options only
         {"cache-dir-location"  , no_argument, NULL, CACHEDIR},
-        {"install-dir-location", no_argument, NULL, INSTALLDIR}
+        {"install-dir-location", no_argument, NULL, INSTALLDIR},
     };
     // clang-format on
 
-    int opt_handler = getopt_long(argc, argv, "d:hi:r:s:uq:", opt_list, NULL);
+    int opt_handler = getopt_long(argc, argv, "d:hi:r:s:u:q:y", opt_list, NULL);
 
     switch (opt_handler) {
     case 's':
         matrix::search_pkg(optarg);
         break;
     case 'u':
-        matrix::update_pkg_list();
+        matrix::update_pkg(optarg);
         break;
     case 'd':
         matrix::download_pkg(optarg);
@@ -70,6 +71,8 @@ int main(int argc, char **argv) {
     case 'q':
         matrix::query_pkg(optarg);
         break;
+    case 'y':
+        matrix::sync_pkg_list();
     // long options
     case CCACHE:
         matrix::cache_clear();
